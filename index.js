7446f0ef66a4f8223f98c834b4a449c835a7ceab
ADITYA PRABHAT GUPTA <aditygupta2474@gmail.com>
2026-03-11 22:11:14 +0530

    updated

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use("/api/tasks", taskRoutes);
 app.use("/api/notices", noticeRoutes);
 initialiseDatabase();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server Started on port ${PORT}`);
